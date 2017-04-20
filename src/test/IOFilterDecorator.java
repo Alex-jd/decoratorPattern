ab9e31330d8ad7f894070d4ff17bbf63ffef24d1
alex_jd <twoalexan@mail.ru>
2017-04-20 23:14:48 +0500

    implement IOFilterDecorator

---
+++ src/test/IOFilterDecorator.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import IODecorator.LowerCaseInputStream;
+
+public class IOFilterDecorator {
+	
+	public static void main (String[] args) throws IOException {
+		int c;
+		try {
+			InputStream in = new LowerCaseInputStream (new BufferedInputStream (new FileInputStream ("test.txt")));
+			
+			while ( (c = in.read()) >= 0) {
+				System.out.print( (char) c);
+			}
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
